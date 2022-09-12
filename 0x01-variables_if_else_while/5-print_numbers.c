bc76492779353ba8b7b26c9d2cde70f20ee7edb4
Gabriel Armah <jiggypapa@gmail.com>
2022-09-12 15:04:45 -0700

    5. Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point into program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	if (num = 0 ; num < 10 ; num++)
+	{
+		printf("%d", num);
+	}
+	printf('\n');
+	return (0);
+}
