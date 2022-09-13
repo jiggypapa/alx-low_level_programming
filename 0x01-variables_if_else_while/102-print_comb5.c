e9290e870be7b5781f05290d5a6f02e81b02ba35
Gabriel Armah <jiggypapa@gmail.com>
2022-09-12 18:54:00 -0700

    102. Print Comb 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ int main(void)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
-			putchar(' ');
+			/*putchar(' ');*/
 
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
