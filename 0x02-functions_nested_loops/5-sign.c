77e4c239148b543f6b1a0f2540ae8b12078351c6
Gabriel Armah <jiggypapa@gmail.com>
2022-09-14 06:08:08 -0700

    5. Print Signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,14 +12,11 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
-	{
-		return (0);
-	}
 	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
-	return (0);
+	else
+		return (0);
 }
