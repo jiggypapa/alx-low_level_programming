9244f9a8dc0808da7d83378cc21399354d1f5743
Gabriel Armah <jiggypapa@gmail.com>
2022-09-14 20:30:23 -0700

    101. natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point compute and print
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, r;
+
+	for (i = 0; i < 1024; 1++)
+	{
+		if (i % 3 = 0 || i % 5 = 0)
+		{
+			r += i;
+		}
+	}
+	printf("%d\n", r);
+	return (0);
+}
