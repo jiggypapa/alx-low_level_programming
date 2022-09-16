6045931dbe64697c7785ee49fadc82f9a2052c9f
Gabriel Armah <jiggypapa@gmail.com>
2022-09-15 17:38:24 -0700

    0. main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point for program to test a function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is a readme file containing details of the project 0x03 C Debugging
