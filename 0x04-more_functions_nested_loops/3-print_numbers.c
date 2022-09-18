9f984b8e92c48a3c15db113cbbcb457497a44d89
Gabriel Armah <jiggypapa@gmail.com>
2022-09-18 00:07:13 -0700

    3. numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,12 @@
 /**
  * print_numbers - prints 0 to 9 integers
  *
- * Return: nothing
+ * Return: void
  */
 void print_numbers(void)
 {
 	int n;
-	
+
 	for (n = 48; n < 58; n++)
 	{
 		_putchar(n);
