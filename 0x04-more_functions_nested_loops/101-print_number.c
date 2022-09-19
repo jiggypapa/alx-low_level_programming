95f219cbc9b59915709c1362cb98eba6c9997363
Gabriel Armah <jiggypapa@gmail.com>
2022-09-19 09:48:00 -0700

    101. print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,11 +3,12 @@
 /**
  * print_number - print a number
  * @n: the number to print
- * Return: void
  */
+
 void print_number(int n)
 {
 	unsigned int x;
+
 	x = n;
 
 	if (n < 0)
@@ -15,9 +16,11 @@ void print_number(int n)
 		_putchar(45);
 		x = -n;
 	}
+
 	if (x / 10)
 	{
 		print_number(x / 10);
 	}
+
 	_putchar((x % 10) + '0');
 }
