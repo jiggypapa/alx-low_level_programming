e5d4e41803dd15b907bb08836c24819ba58d4dcc
Gabriel Armah <jiggypapa@gmail.com>
2022-09-19 21:16:56 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - copy strings
+ * @dest: string parameter
+ * @src: string parameter
+ * Return: returns a character
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+
+	return (dest);
+}
