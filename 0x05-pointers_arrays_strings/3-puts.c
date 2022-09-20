a084ff550a83dd74a1731e840cd2459281734876
Gabriel Armah <jiggypapa@gmail.com>
2022-09-19 19:14:47 -0700

    3. puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,12 @@
 #include "main.h"
-#include <stdio>
+#include <stdio.h>
+
 /**
  * _puts - print a string followed by a new line
  * @str: string to print parameter
  * Return: nothing
  */
+
 void _puts(char *s)
 {
 	int i = 0;
