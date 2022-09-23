d35bf79473571e32632c290ac9f6f02c0f3b925e
Gabriel Armah <jiggypapa@gmail.com>
2022-09-23 03:49:44 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Check code
+ *
+ * Return: Always 0
+ */
+
+int main(void)                                                                                     
+{
+        char s1[98] = "Hello ";
+        char s2[] = "World!\n";
+        char *p;
+        printf("%s\n", s1);
+        printf("%s", s2);
+        p = _strncat(dest, src, n);
+        printf("%s\n", s1);
+        printf("%s", s2);
+        printf("%s", p);
+
+	return (0);
+}
