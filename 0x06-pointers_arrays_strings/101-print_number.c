edaca8e4f8fd45e6ae0eb3059c7f51d494eccf63
Gabriel Armah <jiggypapa@gmail.com>
2022-09-23 07:01:50 -0700

    101. print number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_number -  checks for checks for a digit (0 through 9).
@@ -12,22 +13,23 @@ void print_number(int n)
 	int m, b;
 
 	b = 10;
+
 	if (n < 10 && n >= 0)
 	{
-		_putchar (n + '0');
+		_putchar(n + '0');
 	}
 	else if (n > -10 && n < 0)
 	{
 		n = n - 2 * n;
 		_putchar('-');
-		_putchar (n + '0');
+		_putchar(n + '0');
 	}
 	else
 	{
 		if (n < 0)
 		{
 			n = n * -1;
-			_putchar ('-');
+			_putchar('-');
 		}
 		z = n;
 		while (z / b > 9)
@@ -38,7 +40,7 @@ void print_number(int n)
 		{
 			m = z / b;
 			z = z % b;
-			_putchar (m + '0');
+			_putchar(m + '0');
 			b = b / 10;
 		}
 	}
