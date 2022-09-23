bc91449c2382aa5ee873b83cc91a5134555c3e4f
Gabriel Armah <jiggypapa@gmail.com>
2022-09-22 19:28:34 -0700

    0.strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,11 +11,11 @@ char *_strcat(char *dest, char *src)
        	int c = 0;
 	int n = 0;
 	
-	while (*dest[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	while (*src[c] != '\0')
+	while (src[c] != '\0')
 	{
 		c++;
 	}
