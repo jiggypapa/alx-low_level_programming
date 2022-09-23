573d2df19bd72cee8c3d82477c9b0689f1c83e73
Gabriel Armah <jiggypapa@gmail.com>
2022-09-23 04:22:33 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,7 +3,7 @@
 * _strncpy – Concatenate two strings but add number of bytes
 * @dest: destination string copy buffer
 * @src: source string
-* @n: max number of byte copied
+* @n: integer parameter max number of byte copied
 * Return: Always 0
 */
 char *_strncpy(char *dest, char *src, int n)
@@ -11,10 +11,13 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
 		dest[i] = src[i];
-
+	}
 	for ( ; i < n; i++)
+	{
 		dest[i] = '\0';
+	}
 
 	return (dest);
 }
