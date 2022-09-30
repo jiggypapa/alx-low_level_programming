d79454b1ba92f9b182904fc947fcce32531d23fa
Gabriel Armah <jiggypapa@gmail.com>
2022-09-30 15:51:27 -0700

    3. mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,11 +11,18 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	int a, b;
+
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+		return (0);
+	}	
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	return (0);
 }
