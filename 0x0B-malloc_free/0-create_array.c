db75f14edcefb670d0fd29bdc6be55dde8db6492
Gabriel Armah <jiggypapa@gmail.com>
2022-10-04 19:54:49 -0700

    0. create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,13 +14,12 @@ char *create_array(unsigned int size, char c)
 	unsigned int index;
 
 	if (size == 0)
-	{
 		return (NULL);
 	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
 		return (NULL);
 	for (index = 0; index < size; index++)
-		c = array[index];
+		array[index] = c;
 	return (array);
 }
