73fd2044f4345b9f7e0f68ee1fa2657ae7059d15
Gabriel Armah <jiggypapa@gmail.com>
2022-10-13 21:46:34 -0700

    100. main opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,4 @@
-#include <stddio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -30,12 +30,12 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < bytes; i++)
 	{
-		if (i == bytes - i)
+		if (i == bytes - 1)
 		{
 			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		printf("%02hhx", arr[i]);
+		printf("%02hhx ", arr[i]);
 	}
-	return;
+	return (0);
 }
